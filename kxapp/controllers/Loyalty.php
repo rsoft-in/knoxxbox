7e7ed0bc5681f97d644d047a139c0a06b26d6209
Rajesh Menon <rajeshkuttiyilm@gmail.com>
2021-10-29 08:19:14 +0530

    loyalty module

---
+++ kxapp/controllers/Loyalty.php
@@ -0,0 +1,83 @@
+<?php
+
+class Loyalty extends RS_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('loyalty_model');
+    }
+
+    public function index()
+    {
+        echo 'Invalid Access!';
+    }
+
+    public function get()
+    {
+        $data = json_decode($this->input->post('postdata'));
+        $filt = "";
+        $item_array = array();
+        if (!empty($data->bid))
+            $filt .= "AND (loyalty_b_id = '" . $data->bid . "')";
+
+        $loyalty = $this->loyalty_model->getLoyalty($filt, 'loyalty_modified DESC', $data->pn * $data->ps, $data->ps);
+        if ($loyalty->num_rows() > 0) {
+            foreach ($loyalty->result() as $row) {
+                $_item = array(
+                    'loyalty_id' => $row->loyalty_id,
+                    'loyalty_default' => $row->loyalty_default,
+                    'loyalty_params' => $row->loyalty_params,
+                    'loyalty_modified' => $row->loyalty_modified
+                );
+                $item_array[] = $_item;
+            }
+        }
+        echo json_encode($item_array);
+    }
+
+    public function getById()
+    {
+        $id = $this->input->post('id');
+        $res = $this->loyalty_model->getLoyaltyById($id);
+        if ($res->num_rows() == 1) {
+            $prow = $res->row();
+            $dataArray = array(
+                'loyalty_id' => $prow->loyalty_id,
+                'loyalty_default' => $prow->loyalty_default,
+                'loyalty_params' => $prow->loyalty_params,
+                'loyalty_modified' => $prow->loyalty_modified
+            );
+            echo json_encode($dataArray);
+        } else {
+            echo 'NOT FOUND';
+        }
+    }
+
+    public function upd() {
+		$data = json_decode ( $this->input->post ( 'postdata' ) );
+		$guid = $this->utility->guid ();
+		if ($data->mode === "add") {
+			$this->loyalty_model->insert ( $guid, $data->bid, $data->df, $data->param );
+			if ($this->db->affected_rows () > 0) {
+				echo "SUCCESS";
+			} else
+				echo "Unable to process request";
+		} else {
+			$this->loyalty_model->update ( $data->id, $data->bid, $data->df, $data->param );
+			if ($this->db->affected_rows () > 0) {
+				echo "SUCCESS";
+			} else
+				echo "Unable to process request";
+		}
+	}
+
+    public function del() {
+		$data = json_decode ( $this->input->post ( 'postdata' ) );
+		$this->loyalty_model->delete ( $data->id );
+		if ($this->db->affected_rows () > 0) {
+			echo "SUCCESS";
+		} else
+			echo "Unable to process request";
+	}
+}
---
+++ kxapp/models/Loyalty_model.php
@@ -0,0 +1,73 @@
+<?php
+
+class Loyalty_model extends CI_Model
+{
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function getLoyalty($filter, $sort, $pn, $ps)
+    {
+        $this->db->select('*');
+        $this->db->from('loyalty');
+        $this->db->where("(1=1) " . $filter);
+        $this->db->order_by($sort);
+        $this->db->limit($ps, $pn);
+        $query = $this->db->get();
+        return $query;
+    }
+
+    public function getLoyaltyCount($filter)
+    {
+        $this->db->select('*');
+        $this->db->from('loyalty');
+        $this->db->where("(1=1) " . $filter);
+        return $this->db->count_all_results();
+    }
+
+    public function getLoyaltyById($loyalty_id)
+    {
+        $this->db->select('*');
+        $this->db->from('loyalty');
+        $this->db->where('loyalty_id', $loyalty_id);
+        $query = $this->db->get();
+        return $query;
+    }
+
+    public function insert($loyalty_id, $loyalty_b_id, $loyalty_default, $loyalty_params)
+    {
+        $idata = array(
+            'loyalty_id' => $loyalty_id,
+            'loyalty_b_id' => $loyalty_b_id,
+            'loyalty_default' => $loyalty_default,
+            'loyalty_params' => $loyalty_params,
+            'loyalty_modified' => mdate("%Y-%m-%d %H:%i:%s", time())
+        );
+        $this->db->insert('loyalty', $idata);
+    }
+
+    public function update($loyalty_id, $loyalty_params)
+    {
+        $idata = array(
+            'loyalty_params' => $loyalty_params,
+            'loyalty_modified' => mdate("%Y-%m-%d %H:%i:%s", time())
+        );
+        $this->db->where('loyalty_id', $loyalty_id);
+        $this->db->update('loyalty', $idata);
+    }
+
+    public function setDefault($loyalty_id)
+    {
+        $idata = array(
+            'loyalty_default' => "(loyalty_id = '" . $loyalty_id . "')",
+            'loyalty_modified' => mdate("%Y-%m-%d %H:%i:%s", time())
+        );
+        $this->db->update('loyalty', $idata);
+    }
+
+    public function delete($loyalty_id) {
+		$this->db->where ( 'loyalty_id', $loyalty_id );
+		$this->db->delete ( 'loyalty' );
+	}
+}
